426bc519192daa8cec0cf3196f7f9f8349121198
Rayssa Alves <129634549+rayss4lves@users.noreply.github.com>
2024-05-23 17:35:34 -0300

    Add files via upload

--- banco.py
+++ banco.py
@@ -1,59 +1,50 @@
-def menu():
-    print('\n=============================================')
-    print('Informe o que deseja fazer:')
-    print('1 - Depositar\n2 - Sacar\n3 - Extrato\n4 - Sair')
-    print('=============================================\n')
-    return int(input('= '))
- 
-extrato = ''
-limite = 1000
-numero_saques = 0
-LIMITE_SAQUES = 5
-saldo = 0
- 
-while True:   
-    op = menu()
-    
-    if op == 1:
-        print('\n---------------------------------------------\n')
-        valor = float(input('Informe o valor que deseja depositar: '))
-        if valor > 0:
-            saldo += valor
-            extrato += f'Deposito de: {valor:.2f}\n'
-            
-        else:
-            print('Operação falhou, tente novamente!!\n')
-        print('\n---------------------------------------------\n')
-    elif op == 2:
-        print('\n---------------------------------------------\n')
-        
-        saque_excedido = numero_saques >= LIMITE_SAQUES
-        if saque_excedido == False:
-            valor = float(input('Informe o valor que deseja sacar: '))
-    
-            saldo_excedido = valor > saldo
-            limite_excedido = valor > limite
-            
-            if saldo_excedido: 
-                print('Operação inválida! Saldo insuficiente!\n ')
-            elif limite_excedido:
-                print('Operação inválida! Excedeu o limite de saque!\n ')   
-            else:
-                saldo-=valor
-                extrato+= f'Saque de {valor:.2f}\n'
-                numero_saques+=1 
-            print('\n---------------------------------------------\n')
+import conta as fco
 
-        else:
-            print('Operação inválida! O numero de saques permitidos foi excedido!\n ')
-    elif op == 3:
-        print('\n-------------------EXTRATO-------------------\n')
-        print(extrato)
-        print(f'Saldo total: {saldo:.2f}')
-        print('\n---------------------------------------------\n')
-    elif op == 4:
-        print('Programa encerrado...\n ')
-        break
+def depositar(valor, saldo, extrato):
+    if valor > 0:
+        saldo += valor
+        extrato.append(f'Deposito de: {valor:.2f}\n')
+        print('Deposito relizado com sucesso!\n')
+               
     else:
-        print('Opção inválida!!\n')
+        print('Operação falhou, tente novamente!!\n')
+    return saldo, extrato
 
+
+#função para sacar o valor de uma determinada conta
+def sacar(saldo, extrato, numero_saques, LIMITE_SAQUES, limite):
+    saque_excedido = numero_saques >= LIMITE_SAQUES
+    if saque_excedido == False:
+        valor = float(input('Informe o valor que deseja sacar: '))
+        
+        saldo_excedido = valor > saldo
+        limite_excedido = valor > limite
+                
+        if saldo_excedido: 
+            print('Operação inválida! Saldo insuficiente!\n ')
+        elif limite_excedido:
+            print('Operação inválida! Excedeu o limite de saque!\n ')   
+        else:
+            saldo-=valor
+            extrato+= f'Saque de {valor:.2f}\n'
+            numero_saques+=1 
+            print('Saque realizado com sucesso!')
+    else:
+        print('Operação inválida! O numero de saques permitidos foi excedido!\n ')
+
+    return saldo, extrato, numero_saques
+ 
+ 
+def imprimir_extrato(contas):
+    num_conta = int(input('Informe o numero da conta: '))
+            
+    conta = fco.verifica_numero_conta(contas, num_conta)
+    if conta:
+        print('\n-------------------EXTRATO-------------------\n')
+        print(conta['extrato'])
+        print(conta['saldo'])
+        
+        print('\n---------------------------------------------\n')
+    else:
+        print('Conta não encontrada!!')
+ 
